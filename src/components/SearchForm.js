f5a8ec476a476c27aef4e6434afc5bfb7238e0f4
Riley Hoffman <riley.n.hoffman@gmail.com>
2026-02-13 15:12:31 -0500

    Add SearchForm.js and reference searchParams for getRecipes query

---
+++ src/components/SearchForm.js
@@ -0,0 +1,8 @@
+export const SearchForm = () => {
+  return (
+    <form action='/' method='GET' role='search'>
+      <input type='search' name='search' />
+      <button type='submit'>Search</button>
+    </form>
+  );
+};
